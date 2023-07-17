598737030165dcfa8e48154ef951b5f9130627ad
anapaunavas <anapaulahong@gmail.com>
2023-07-16 21:56:16 -0500

    parte 1

---
+++ ItemDataJ.java
@@ -0,0 +1,47 @@
+public class ItemDataJ{
+    Object originalValue;
+
+    public ItemDataJ(Object originalValue){
+            this.originalValue = originalValue;
+        }
+
+    public String getType(){
+        if(originalValue.getClass().equals(String.class)) {
+            return "cadena";
+        } else if (originalValue.getClass().equals(Integer.class)) {
+            return "entero";
+        } else if (originalValue.getClass().equals(Boolean.class)) {
+            return "booleano";
+        } else {
+            return null; 
+        }
+        
+    }
+
+    public String getInfo() {
+        if (originalValue.getClass().equals(String.class)) {
+            String stringValue = (String) originalValue;
+            return "L" + stringValue.length();
+        } else if (originalValue.getClass().equals(Integer.class)) {
+            int intValue = (Integer) originalValue;
+            if (intValue % 10 == 0) {
+                return "M10";
+            } else if (intValue % 5 == 0) {
+                return "M5";
+            } else if (intValue % 2 == 0) {
+                return "M2";
+            } else {
+                return null;
+            }
+        } else if(originalValue.getClass().equals(Boolean.class)){
+            boolean boolValue = (Boolean) originalValue;
+            return boolValue ? "Verdadero" : "Falso";
+        } else {
+            return null;
+        }
+        }
+
+    public static void Main(String[] args){
+        
+    }
+}
